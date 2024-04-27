bdee7209f0063f76c919dc67fbf2f8b2a59fa5f7
Erick Poleto <erick-poleto@hotmail.com>
2024-04-27 16:30:04 -0300

    Complete

--- src/scene.ts
+++ src/scene.ts
@@ -15,5 +15,60 @@ import {
 } from "three";
 
 export function createScene(renderer: WebGLRenderer) {
-  // TODO: Create a scene and build a WebXR app!
+  const scene = new Scene()
+
+  const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.2, 20 );
+
+  const planeMarker = createPlaneMarker();
+
+  scene.add(planeMarker);
+
+  let koalaModel: Object3D;
+  
+  const gltfLoader = new GLTFLoader();
+
+  gltfLoader.load("../assets/models/koala.glb", (gltf: GLTF) => {
+    koalaModel = gltf.scene.children[0];
+  });
+
+  const controller = renderer.xr.getController(0);
+  scene.add(controller);
+
+  controller.addEventListener("select", onSelect);
+
+  function onSelect() {
+    if (planeMarker.visible) {
+      const model = koalaModel.clone();
+
+      model.position.setFromMatrixPosition(planeMarker.matrix);
+
+      // Rotate the model randomly to give a bit of variation to the scene.
+      model.rotation.y = Math.random() * (Math.PI * 2);
+      model.visible = true;
+
+      scene.add(model);
+    }
+  }
+  
+  const ambientLight = new AmbientLight(0xffffff, 1.0);
+  scene.add(ambientLight);
+
+  const renderLoop = (timestamp: number, frame?: XRFrame) => {
+    if (renderer.xr.isPresenting) {
+      if (frame) {
+        handleXRHitTest(renderer, frame, (hitPoseTransformed: Float32Array) => {
+          if (hitPoseTransformed) {
+            planeMarker.visible = true;
+            planeMarker.matrix.fromArray(hitPoseTransformed);
+          }
+        }, () => {
+          planeMarker.visible = false;
+        })
+      }
+      renderer.render(scene, camera);
+    }
+  }
+
+  
+  renderer.setAnimationLoop(renderLoop);
 }
